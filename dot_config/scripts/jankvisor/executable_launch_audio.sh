1a27e02f662785840004173e6960f97c5fee1178
ripplefcl <ripple@ripplefcl.com>
2023-10-26 23:07:58 +0100

    im a doofusim a doofusim a doofusim a doofusim a doofus

--- dot_config/scripts/jankvisor/executable_launch_audio.sh
+++ dot_config/scripts/jankvisor/executable_launch_audio.sh
@@ -1,1 +1,1 @@
-ssh vmaudio@10.0.1.30 -X -i ~/.ssh/id_hva_e25519.pub pavucontrol
+ssh vmaudio@10.0.1.30 -X -i ~/.ssh/id_hva_ed25519 pavucontrol
--- dot_config/scripts/jankvisor/executable_launch_bt.sh
+++ dot_config/scripts/jankvisor/executable_launch_bt.sh
@@ -1,1 +1,1 @@
-ssh vmaudio@10.0.1.30 -X -i ~/.ssh/id_hva_e25519.pub bash "killall -q 'blueman-applet'; killall -q 'blueman-manager'; sleep 1; blueman-applet & blueman-manager; killall -q 'blueman-applet'"
+ssh vmaudio@10.0.1.30 -X -i ~/.ssh/id_hva_ed25519 bash "killall -q 'blueman-applet'; killall -q 'blueman-manager'; sleep 1; blueman-applet & blueman-manager; killall -q 'blueman-applet'"
